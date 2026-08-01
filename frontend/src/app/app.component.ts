33057b8157167e339ac05179e5a0e0d184bb5612
Valentina Gonzalez <valenavg12@gmail.com>
2026-08-01 19:31:19 -0400

    Organizar la estructura del portafolio en componentes

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {HeroComponent} from './hero/hero.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProjectsComponent],
+  imports: [RouterOutlet, ProjectsComponent, ContactComponent, NavbarComponent, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
